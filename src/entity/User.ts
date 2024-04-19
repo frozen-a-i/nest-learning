67e41f5e5adac25b7406c567e58b1c225e89420a
Anakhon Ismoilova <anakhonismoilova@Anakhons-MacBook-Pro.local>
2024-04-19 11:32:53 +0500

    changes

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,34 +1,34 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+// import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity()
-export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+// @Entity()
+// export class User {
+//   @PrimaryGeneratedColumn()
+//   id: number;
 
-  @Column({ length: 50 })
-  name: string;
+//   @Column({ length: 50 })
+//   name: string;
 
-  @Column({ length: 20 })
-  phone: string;
-}
+//   @Column({ length: 20 })
+//   phone: string;
+// }
 
-@Entity()
-export class admins {
-  @PrimaryGeneratedColumn()
-  id: number;
+// @Entity()
+// export class admins {
+//   @PrimaryGeneratedColumn()
+//   id: number;
 
-  @Column({ length: 50 })
-  name: string;
-}
+//   @Column({ length: 50 })
+//   name: string;
+// }
 
-@Entity()
-export class products {
-  @PrimaryGeneratedColumn()
-  id: number;
+// @Entity()
+// export class products {
+//   @PrimaryGeneratedColumn()
+//   id: number;
 
-  @Column({ length: 50 })
-  name: string;
+//   @Column({ length: 50 })
+//   name: string;
 
-  @Column({ type: 'real' })
-  price: number;
-}
+//   @Column({ type: 'real' })
+//   price: number;
+// }
